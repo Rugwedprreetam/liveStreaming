57fcb5df331188bc08cd16745a1b9285e5c559a5
Rugwedprreetam <rugwedprreetam@gmail.com>
2023-03-24 17:34:43 +0530

    UserID Update

--- server.js
+++ server.js
@@ -13,14 +13,15 @@ app.use(express.static("public"));
 app.get("/stream",(req,res)=>{
 const appID = 549767164;
 const secret = '1f3b7487839376d767c03ef823f4d131';
-const userId = "room111"; 
-const userName = "user123"
+const roomID = "room111"; 
+const userID = "user"+Math.floor((Math.random()*1000))
+const userName = "user123"+Math.floor((Math.random()*1000));
 const effectiveTimeInSeconds = 3600; 
 const payload = '';
 
-const token = generateToken04(appID, userId, secret, effectiveTimeInSeconds, payload);
+const token = generateToken04(appID, userID, secret, effectiveTimeInSeconds, payload);
 console.log(token);
-res.render("stream",{roomID:userId,kitToken:token,userName:userName,appID:appID});
+res.render("stream",{roomID:roomID,userID:userID,kitToken:token,userName:userName,appID:appID});
 // res.send(__dirname+"/views/stream.html");
 console.log("rrr");
 });
